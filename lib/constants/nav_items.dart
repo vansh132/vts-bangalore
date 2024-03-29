ec0c9392da7f8db91d548b904fe8884c8843d2be
vansh132 <shah.vansh132@gmail.com>
2024-03-29 15:49:23 +0530

    desktop setup initiated

--- lib/constants/nav_items.dart
+++
@@ -1,6 +0,0 @@
-List<String> navItems = [
-  "Home",
-  "Skills",
-  "Projects",
-  "Contact us",
-];
---
+++ lib/pages/desktop/exCom.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExComScreen extends StatelessWidget {
+  const ExComScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
